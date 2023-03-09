f98b0009289427064402fe9e816ce6b43ce45b7a
Maximiliano Cortez <maxi_cortez79@hotmail.com>
2023-03-09 08:56:03 -0300

    Primer Commit

---
+++ src/views/Login.js
@@ -0,0 +1,91 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../context/UserProvider";
+import Swal from "sweetalert2";
+import { Navigate } from "react-router-dom";
+// import axios from "axios";
+
+const Login = () => {
+    const [_usuario, setUsuario] = useState("");
+    const [_clave, setClave] = useState("");
+    const { user, iniciarSession } = useContext(UserContext);
+
+    if (user != null) {
+        return <Navigate to="/" />;
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        let request = {
+            username: _usuario,
+            password: _clave,
+        };
+
+        fetch("http://localhost:3000/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(request),
+        })
+            .then((response) => {
+                return response.ok ? response.json() : Promise.reject(response);
+            })
+            .then((dataJson) => {
+                if (dataJson.error === true) {
+                    Swal.fire("Opps!", "No se encontro el usuario", "error");
+                } else {
+                    iniciarSession(dataJson);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                Swal.fire("Opps!", "No se pudo iniciar sessión", "error");
+            });
+    };
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-xl-10 col-lg-12 col-md-9">
+                    <div className="card o-hidden border-0 shadow-lg my-5">
+                        <div className="card-body p-0">
+                            <div className="row">
+                                <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                                <div className="col-lg-6">
+                                    <div className="p-5">
+                                        <div className="text-center">
+                                            <h1 className="h4 text-gray-900 mb-4">Bienvenido</h1>
+                                        </div>
+                                        <form className="user" onSubmit={handleSubmit}>
+                                            <div className="form-group">
+                                                <input
+                                                    type="text"
+                                                    className="form-control form-control-user"
+                                                    aria-describedby="emailHelp"
+                                                    placeholder="Nombre de usuario"
+                                                    value={_usuario}
+                                                    onChange={(e) => setUsuario(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <input type="password" className="form-control form-control-user" placeholder="Contraseña" value={_clave} onChange={(e) => setClave(e.target.value)} />
+                                            </div>
+                                            <button type="submit" className="btn btn-primary btn-user btn-block">
+                                                {" "}
+                                                Ingresar{" "}
+                                            </button>
+                                        </form>
+                                        <hr></hr>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
